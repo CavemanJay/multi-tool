1f08e7bc6e0cd596804cebd687004b757329b92f
Jay Cuevas <Jordan.Cuevas@precisecal.com>
2021-01-19 02:00:38 -0500

    Update logging in main

--- main.go
+++ main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
-	"log"
 	"os"
 
 	"github.com/JayCuevas/gogurt/cli"
+	"github.com/op/go-logging"
 )
 
 func main() {
 	app := cli.InitApp()
 	err := app.Run(os.Args)
 	if err != nil {
+		log := logging.MustGetLogger("gogurt")
 		log.Fatal(err)
 	}
 }
